2fdb795d49f08059be9fad0b3f8d064d0c2eea85
x-xiho <x_xiho3o@naver.com>
2023-09-27 18:17:04 +0900

    test

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
-import React, { useState } from 'react';
+import './App.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [get, setGet] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleget = (e) => {
+    axios.get('http://localhost:3000/api/getFormData')
+      .then(response => {
+        setData(response.data);
+        setGet(!get);
+      })
+
+      .catch(error => {
+        console.error('데이터를 불러오는 중 오류가 발생했습니다.', error);
+      });
+  };
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
-    // 필요한 다른 사용자 데이터 추가
+    radio: '',
+    // 나머지 필드들 추가
   });
 
+
+  // 버튼 누르면 데이터 전송
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:3000/api/saveFormData', formData)
+      .then((response) => {
+        console.log(response.data);
+        alert('데이터가 성공적으로 저장');
+      })
+
+      .catch((error) => {
+        console.error(error);
+        alert('데이터 저장 중 오류가 발생');
+      });
+
+  };
+
+  // 변경되는 데이터를 formData에 저장
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -16,84 +52,56 @@ function App() {
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Axios를 사용하여 백엔드로 POST 요청 보내기
-      const response = await axios.post('http://localhost:3000/saveToExcel', formData);
-
-      console.log('백엔드 응답:', response.data);
-    } catch (error) {
-      console.error('요청 실패:', error);
-    }
-  };
 
   return (
-    <div className="container">
+    <div className='container'>
 
       {/* form태그 시작 */}
       <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>로그인 데이터</legend>
+
+        <fieldset >
+          <legend>form 데이터 연습</legend>
 
           {/* 아이디 비밀번호 예시 */}
-          아이디: 
-          <input
-            type="email"
-            name="email"
-            placeholder="이메일"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+          이메일: <input type="email" name="email" placeholder="이메일" onChange={handleChange} value={formData.email} />
           <br />
-          비밀번호: 
-          <input
-            type="password"
-            name="password"
-            placeholder="비밀번호"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+          비밀번호: <input type="password" name="password" placeholder="비밀번호" onChange={handleChange} value={formData.password} />
+          <br />
+
+          <h3> 나의 취향은? </h3>
+
+          <label>
+            1. 운동 <input type="radio" value='운동' checked={formData.radio === '운동'} name="radio" onChange={handleChange} />
+          </label>
+
+          <label>
+            2. 미술 <input type="radio" value='미술' checked={formData.radio === '미술'} name="radio" onChange={handleChange} />
+          </label>
+
+          <label>
+            3. 음악 <input type="radio" value='음악' checked={formData.radio === '음악'} name="radio" onChange={handleChange} />
+          </label>
+
+
         </fieldset>
 
-        {/* 여러가지 옵션 선택 데이터들 */}
-        <fieldset>
-          <legend>여러가지 옵션 선택 데이터들</legend>
-          남<input type="radio" name="gender" />
-          여<input type="radio" name="gender" />
 
-          <br />
-          관심사: 연예 <input type="checkbox" name="checkbox1" />
-          스포츠<input type="checkbox" name="checkbox1" />
-          IT<input type="checkbox" name="checkbox1" />
-
-          <br />
-          지역선택<select name="city1">
-            <optgroup label="서울">
-              <option value="songpa">송파구</option>
-              <option value="gangnam">강남구</option>
-              <option value="gangdong">강동구</option>
-            </optgroup>
-            <optgroup label="경기도">
-              <option value="seongnam-si">성남시</option>
-              <option value="yongin-si">수원시</option>
-              <option value="anyang-si">안양시</option>
-            </optgroup>
-          </select>
-          <br />
-
-          재산범위 <input type="range" min="0" max="100" step="10" name="range" />
-        </fieldset>
-
-        {/* 제출 버튼 */}
         <input type="submit" value="로그인" />
         <input type="reset" value="리셋" />
+        <input type="button" value="내정보" onClick={handleget} />
       </form>
+
+      {get ? (
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{item.email}, {item.password}, {item.radio}</li>
+          ))}
+        </ul>
+      ) : null}
+
+
     </div>
   );
 }
 
 export default App;
-
